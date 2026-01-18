3662c6d7d49c03e0c1f314fc45bb0891d780acc2
IrumShehryar <irum.shehryar@gmail.com>
2026-01-18 15:01:59 +0200

    Add FastAPI basic endpoints

---
+++ fast-api-practice/main.py
@@ -0,0 +1,40 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[float, None] = None
+    
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI application!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_price": item.price, "item_id": item_id}
+
+
+#Additional endpoint to demonstrate Enum usage
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
